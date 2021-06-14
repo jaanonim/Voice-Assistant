cf8cbee985faa78e1c7f16c01f1688666498135d
jaanonim <mat8mro@gmail.com>
2021-06-14 16:43:55 +0200

    missing args

--- utilities/io.py
+++ utilities/io.py
@@ -5,18 +5,17 @@ from .speaker import Speaker
 
 
 class IO:
-    def __init__(self, func):
-        self.func = func
+    def __init__(self):
         self.inputVoice = Settings.getInstance().get("inputVoice")
 
     def run(self):
 
         if self.inputVoice:
-            Listener(self.func, Settings.getInstance().get("invocation")).setup()
+            Listener(Settings.getInstance().get("invocation")).setup()
             while True:
                 pass
         else:
-            l = Listener(self.func, None)
+            l = Listener(None)
             while True:
                 inp = input("INPUT: ")
                 if inp:
