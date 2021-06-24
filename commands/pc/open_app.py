9c745ef737b429d06c3d44e75c8e846f01508d0c
jaanonim <mat8mro@gmail.com>
2021-06-24 11:48:47 +0200

    fixing open app

--- commands/pc/open_app.py
+++ commands/pc/open_app.py
@@ -1,6 +1,7 @@
-import webbrowser
+import time
 
-import winapps
+import keyboard
+import psutil
 from classes.command import Command
 
 
@@ -8,9 +9,9 @@ class OpenApp(Command):
     def __init__(self):
         super().__init__()
         self.aliases = [
+            "open up {name|what app should I open?}",
             "open app {name|what app should I open?}",
             "open application {name|what application should I open?}",
-            "open program {name|what program should I open?}",
             "start app {name|what app should I open?}",
             "start application {name|what application should I open?}",
             "start program {name|what program should I open?}",
@@ -18,16 +19,21 @@ class OpenApp(Command):
         self.target = "pc"
 
     def _execute(self):
-        #TODO: Implement search in win - good luck
-        return False, "Not implemented.", None
         n = self.values.get("name")
-        print(n)
-        ok = False
-        for app in winapps.search_installed(n):
-            ok = True
-            print(app)
-            break
-        if ok:
+        if self.is_power_launcher_running():
+            keyboard.press_and_release("alt+space")
+            time.sleep(0.1)
+            keyboard.press_and_release("backspace")
+            keyboard.write(n)
+            time.sleep(0.1)
+            keyboard.press_and_release("enter")
+
             return False, f"OK. Opening {n}", None
-        else:
-            return False, f"Not found app {n}.", None
+
+        return False, "To open apps you need to have running Power Launcher.", None
+
+    def is_power_launcher_running(self):
+        for p in psutil.process_iter():
+            if "PowerLauncher.exe" == p.name():
+                return True
+        return False
